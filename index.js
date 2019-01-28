c38d4d30f4e8000ebcf2ac8a68734525cc5c28ad
Tracy Ngo <tngo1206@gmail.com>
2019-01-28 01:43:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,6 +8,11 @@ function whisper(string){
 
 function logShout(string){
   console.log (`${string.toUpperCase()}`)
-
 }
+
+function logWhisper(string){
+  console.log(`${string.toLowercase()}`)
+}
+
+function
 
